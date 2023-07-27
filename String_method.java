debcade3d36a9db2754fd9c62404f42459c4259d
SOUMEN MANNA <103374779+soumenprogramming@users.noreply.github.com>
2023-07-27 21:10:25 +0530

    Add files via upload

---
+++ String_method.java
@@ -0,0 +1,16 @@
+public class String_method {
+    public static void main(String[]args){
+        String str="www.abcd.org";
+        String str1=new String(str);
+       // System.out.println(str1.length());
+       //System.out.println(str1.toUpperCase());
+       //System.out.println(str1.toLowerCase());
+    //    System.out.println(str1.trim());
+    //    System.out.println(str1);
+      //System.out.println(str1.substring(3,7));
+      //System.out.println(str1.replace('e','f'));
+      //System.out.println(str1.indexOf("?"));
+      // System.out.println(str1.equals(str));
+      // System.out.println(str1==str);
+    }
+}
