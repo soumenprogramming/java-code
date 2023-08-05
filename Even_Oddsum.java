65e9bef9042ea4faa14198ab6e2e5c31a5c9039a
SOUMEN MANNA <103374779+soumenprogramming@users.noreply.github.com>
2023-08-05 21:19:20 +0530

    Add files via upload

---
+++ Even_Oddsum.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class Even_Oddsum {
+    public static void main(String[]args)
+    {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("enter the number :");
+        int t=sc.nextInt();
+        int evensum=0,oddsum=0;
+        for (int i = 1; i <= t; i++) {
+            if (i % 2 == 0) {
+                evensum += i;
+            } else {
+                oddsum += i;
+            }
+        }
+
+        System.out.println(oddsum);
+        System.out.println(evensum);
+    }
+}
